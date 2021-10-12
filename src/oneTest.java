f06cd136ed1bca670accc2a7706f7ee45996d833
user <ijh2@swempire.co.kr>
2021-10-12 15:50:29 +0900

    second commit

---
+++ src/oneTest.java
@@ -0,0 +1,6 @@
+public class oneTest {
+    public static void main(String[] args){
+        double x = 0.9999999999999999;
+        System.out.println("x = "+x+"\n10x = "+10*x+"\n9x = "+9*x+"\nx = "+x);
+    }
+}
